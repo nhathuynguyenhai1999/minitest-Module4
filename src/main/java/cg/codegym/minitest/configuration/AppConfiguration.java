40ea1945abf5ea2fd2fa9847d9e47d9d180ffe6d
nhathuynguyenhai1999 <nhathuynguyenhai@gmail.com>
2024-06-05 10:49:36 +0700

    changing package uppercase

--- src/main/java/cg/codegym/minitest/configuration/AppConfiguration.java
+++ src/main/java/cg/codegym/minitest/configuration/AppConfiguration.java
@@ -1,5 +1,5 @@
-package cg.codegym.minitest.Configuration;
-import cg.codegym.minitest.Formatter.TypeFormatter;
+package cg.codegym.minitest.configuration;
+import cg.codegym.minitest.formatter.TypeFormatter;
 import cg.codegym.minitest.Service.iml.TypeService;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -43,7 +43,7 @@ import java.util.Properties;
 @EnableWebMvc
 @ComponentScan("cg.codegym.minitest")
 @EnableTransactionManagement
-@EnableJpaRepositories("cg.codegym.minitest.Repository")
+@EnableJpaRepositories("cg.codegym.minitest.repository")
 @EnableSpringDataWebSupport
 @EnableAspectJAutoProxy
 public class AppConfiguration implements WebMvcConfigurer,ApplicationContextAware {
